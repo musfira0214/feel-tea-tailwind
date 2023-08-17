a7921f2a3f500435f446f2ec5036b2ef398be894
musfira0214 <musfirarazzaq17331@gmail.com>
2023-08-17 09:22:16 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './public/*html',
+  ],
+  theme: {
+    extend: {},
+    container:{
+      center: true,
+      padding: '1rem',
+      screens:
+        {
+          lg: '1124px',
+          xl: '1124px',
+          '2xl': '1124px'
+        }
+    },
+  },
+  plugins: [],
+}
+
